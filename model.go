60faf3bed209d6a1d3bb6dd4f76a4f88de01ecc4
Yasin <yasin.kizilkaya@projectbeagle.com>
2016-12-26 12:08:12 +0200

    added models

---
+++ model.go
@@ -0,0 +1,13 @@
+package main
+
+type Version struct {
+	Major       uint8
+	Minor       uint8
+	BuildNumber uint16
+}
+
+type Project struct {
+	Key     string
+	Desc    string
+	Version *Version
+}
