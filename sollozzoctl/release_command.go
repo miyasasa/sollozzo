8507b1542906ce038ba3ef17b7fda43df5c58abd
Burak AYDIN <burak.aydin@projectbeagle.com>
2016-12-27 09:18:00 +0300

    inlined release project onto else block

--- sollozzoctl/release_command.go
+++ sollozzoctl/release_command.go
@@ -5,12 +5,13 @@ import (
 	"github.com/yasinKIZILKAYA/sollozzo/model"
 	"fmt"
 	"encoding/json"
+	"os"
 )
 
 var name string
 
 var releaseCmd = &cobra.Command{
-	Use:   "release [release version]",
+	Use:   "release [projectname] [major, minor, build]",
 	Short: "Release project version",
 	Long:  "Release project version",
 	Run:func(cmd *cobra.Command, args[]string) {
@@ -19,33 +20,37 @@ var releaseCmd = &cobra.Command{
 		} else {
 			name = args[0]
 			op := args[1]
-			dispatch(name, op)
+			var p model.Project
+			store.Get([]byte(name), &p)
+
+			if (model.Project{}) == p {
+				fmt.Print("can not found project !!")
+				os.Exit(1)
+			}
+
+			switch op {
+			case "major":
+				p.Major += 1
+			case "minor":
+				p.Minor += 1
+			case "build":
+				p.BuildNumber += 1
+			default:
+				cmd.Help()
+				os.Exit(1)
+			}
+
+			encode, err := json.Marshal(&p)
+			if err != nil {
+				panic(err)
+			}
+
+			store.Put([]byte(p.Key), encode)
+			fmt.Println(p.Version())
 		}
 	},
 }
 
 func init() {
 	cmdSollozzo.AddCommand(releaseCmd)
-}
-
-func dispatch(projectName string, op string) {
-	var p model.Project
-	store.Get([]byte(projectName), &p)
-
-	switch op {
-	case "major":
-		p.Major += 1
-	case "minor":
-		p.Minor += 1
-	case "build":
-		p.BuildNumber += 1
-	}
-
-	encode, err := json.Marshal(&p)
-	if err != nil {
-		panic(err)
-	}
-
-	store.Put([]byte(p.Key), encode)
-	fmt.Println(p.Version())
 }
