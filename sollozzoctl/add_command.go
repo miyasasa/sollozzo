b0be57d7025b6918e6b88dd29b312b6eb4236e4d
Yasin <yasin.kizilkaya@projectbeagle.com>
2016-12-28 12:17:43 +0200

    # Refactor add command as take project version from user on created project state

--- sollozzoctl/add_command.go
+++ sollozzoctl/add_command.go
@@ -2,7 +2,9 @@ package sollozzoctl
 
 import (
 	"fmt"
-
+	"regexp"
+	"strings"
+	"strconv"
 	"github.com/spf13/cobra"
 	"github.com/yasinKIZILKAYA/sollozzo/model"
 	"github.com/yasinKIZILKAYA/sollozzo/boltdb"
@@ -11,13 +13,15 @@ import (
 func NewAddCommand(store *boltdb.Store) *cobra.Command {
 
 	cmd := &cobra.Command{
-		Use:   "add <project_name>",
+		Use:   "add <project_name> [version as major.minor.build]",
 		Short: "Add new Project",
 		Long:  "Add new Project",
 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			if len(args) != 1 {
-				return fmt.Errorf("\"sollozzo add\" accepts only project name argument.")
+			length := len(args)
+
+			if length > 2 || length == 0 {
+				return fmt.Errorf("\"sollozzo add\" accepts only project name and project version(optional) arguments.")
 			}
 			return runAddCommand(store, args)
 		},
@@ -27,7 +31,27 @@ func NewAddCommand(store *boltdb.Store) *cobra.Command {
 }
 func runAddCommand(store *boltdb.Store, args []string) error {
 
-	var project = &model.Project{Key: args[0], Major: 1, Minor: 0, BuildNumber: 0}
+	var project = &model.Project{};
+
+	if len(args) == 2 {
+
+		reg, err := regexp.MatchString("^(\\d)(.\\d){2}$", args[1])
+
+		if err != nil || reg == false {
+			return fmt.Errorf("Project can not created. Invalid version number, try as 1.2.3 ")
+		}
+		vParameters := strings.Split(args[1], ".")
+
+		major, _ := strconv.ParseUint(vParameters[0], 10, 8)
+		minor, _ := strconv.ParseUint(vParameters[1], 10, 8)
+		build, _ := strconv.ParseUint(vParameters[2], 10, 16)
+
+		project = &model.Project{Key: args[0], Major: uint8(major), Minor: uint8(minor), BuildNumber: uint16(build)}
+
+	} else {
+
+		project = &model.Project{Key: args[0], Major: 1, Minor: 0, BuildNumber: 0}
+	}
 
 	err := store.Put([]byte(args[0]), project)
 
